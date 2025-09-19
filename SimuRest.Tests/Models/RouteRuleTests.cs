138ef5d9814b68ad67c70453684ada3f8084b3c3
Wilhelm Jahn <wilhelmjahn2005@gmail.com>
2025-09-19 17:30:23 +0200

    Add basic init tests for Models

---
+++ SimuRest.Tests/Models/RouteRuleTests.cs
@@ -0,0 +1,21 @@
+using SimuRest.Core.Models;
+
+namespace SimuRest.Tests.Models;
+
+public class RouteRuleTests
+{
+    public static IEnumerable<object[]> InvalidInitArguments =>
+        new List<object[]>
+        {
+            new object[] { null, null },
+            new object[] { new Route(HttpMethod.Post, "/user"), null },
+            new object[] { null, new Func<SimuRequest, SimuResponse>(req => new SimuResponse(1, "a")) },
+        };
+    
+    [Theory]
+    [MemberData(nameof(InvalidInitArguments))]
+    public void Init_RouteOrHandlerIsNull_ThrowsArgumentException(Route route, Func<SimuRequest, SimuResponse> func)
+    {
+        Assert.Throws<ArgumentException>(() => new RouteRule(route, func));
+    }
+}
---
+++ SimuRest.Tests/Models/RouteTests.cs
@@ -0,0 +1,27 @@
+using SimuRest.Core.Models;
+namespace SimuRest.Tests.Models;
+
+public class RouteTests
+{
+    public static IEnumerable<object[]> InvalidRequests =>
+        new List<object[]>
+            {
+                new object[] { null, null },
+                new object[] { null, "" },
+                new object[] { null, "   " },
+                new object[] { HttpMethod.Get, null },
+                new object[] { HttpMethod.Get, "" },
+                new object[] { HttpMethod.Get, "   " },
+                new object[] { HttpMethod.Post, null },
+                new object[] { HttpMethod.Post, "" },
+                new object[] { HttpMethod.Post, "   " },
+                new object[] { null, "/user" },
+            };
+    
+    [Theory]
+    [MemberData(nameof(InvalidRequests))]
+    public void Init_ArgumentsAreEmptyOrNull_ThrowsArgumentException(HttpMethod method, string path)
+    {
+        Assert.Throws<ArgumentException>(() => new Route(method, path));
+    }
+}
