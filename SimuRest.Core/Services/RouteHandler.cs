83525d6a9cb15acdba49b84424d86a3cbaa7d277
Wilhelm Jahn <wilhelmjahn2005@gmail.com>
2025-09-19 22:47:35 +0200

    Implement minimal functions

--- SimuRest.Core/Services/RouteHandler.cs
+++ SimuRest.Core/Services/RouteHandler.cs
@@ -1,6 +1,30 @@
+using System.Reflection.Metadata;
+using SimuRest.Core.Models;
+
 namespace SimuRest.Core.Services;
 
 public class RouteHandler
 {
-    
+    public List<RouteRule> Routes { get; set; } = new();
+
+    public SimuResponse Handle(SimuRequest request)
+    {
+        if (request is null) throw new ArgumentException("Request cannot be null", nameof(request));
+        
+        try
+        {
+            foreach (RouteRule route in Routes)
+            {
+                if (route.Route.Method == request.Method && route.Route.Path == request.Path)
+                    return route.Handler(request);
+            }
+
+            return new SimuResponse(404, "");
+        }
+
+        catch
+        {
+            return new SimuResponse(500, "Internal Server Error");
+        }
+    }
 }
