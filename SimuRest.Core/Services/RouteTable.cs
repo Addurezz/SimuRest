baa2c253216540685297c5d83f8be8f72bdaaa70
Wilhelm Jahn <wilhelmjahn2005@gmail.com>
2025-09-20 11:09:18 +0200

    New class to manage storing and retrieving of RouteRules

---
+++ SimuRest.Core/Services/RouteTable.cs
@@ -0,0 +1,27 @@
+using SimuRest.Core.Models;
+namespace SimuRest.Core.Services;
+
+public class RouteTable
+{
+    public Dictionary<(HttpMethod, string), RouteRule> Routes { get; set; } = new();
+
+    public RouteRule Match(SimuRequest req)
+    {
+        (HttpMethod, string) key = (req.Method, req.Path);
+
+        RouteRule rule;
+
+        if (!Routes.TryGetValue(key, out rule)) throw new KeyNotFoundException();
+
+        return rule;
+    }
+
+    public void Insert(RouteRule rule)
+    {
+        if (rule is null) throw new ArgumentNullException();
+
+        (HttpMethod, string) key = (rule.Route.Method, rule.Route.Path);
+
+        if (!Routes.TryAdd(key, rule)) ;
+    }
+}
