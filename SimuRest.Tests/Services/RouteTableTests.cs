ad8ab626b89579ed735b7019f2f8678df02a70af
Wilhelm Jahn <wilhelmjahn2005@gmail.com>
2025-09-20 11:28:39 +0200

    basic tests for matching and insertion

---
+++ SimuRest.Tests/Services/RouteTableTests.cs
@@ -0,0 +1,59 @@
+using SimuRest.Core.Services;
+using SimuRest.Core.Models;
+
+namespace SimuRest.Tests.Services;
+
+public class RouteTableTests
+{
+    [Fact]
+    public void Insert_RouteDoesExist_DoesNotAddOrOverride()
+    {
+        RouteTable table = new RouteTable();
+        RouteRule rule = new RouteRule(new Route(HttpMethod.Get, "/foo"), 
+            req => new SimuResponse(200, ""));
+        var key = (rule.Route.Method, rule.Route.Path);
+        table.Routes[key] = rule;
+
+        Assert.Single(table.Routes);
+        
+        RouteRule rule2 = new RouteRule(new Route(HttpMethod.Get, "/foo"), 
+            req => new SimuResponse(200, ""));
+        table.Insert(rule2);
+        
+        Assert.Single(table.Routes);
+    }
+
+    [Fact]
+    public void Insert_RouteRuleDoesNotExist_AddsSuccessfully()
+    {
+        RouteTable table = new RouteTable();
+        RouteRule rule = new RouteRule(new Route(HttpMethod.Get, "/foo"), 
+            req => new SimuResponse(200, ""));
+        table.Insert(rule);
+
+        Assert.Single(table.Routes);
+    }
+
+    [Fact]
+    public void Match_RouteRuleExists_ReturnsRouteRule()
+    {
+        RouteTable table = new RouteTable();
+        SimuRequest req = new SimuRequest(HttpMethod.Get, "/foo");
+        RouteRule rule = new RouteRule(new Route(HttpMethod.Get, "/foo"), 
+            req => new SimuResponse(200, ""));
+        var key = (rule.Route.Method, rule.Route.Path);
+        table.Routes[key] = rule;
+
+        Assert.Equal(rule, table.Match(req));
+    }
+
+    [Fact]
+    public void Match_RouteRuleDoesNotExist_ThrowsKeyNotFoundException()
+    {
+        RouteTable table = new RouteTable();
+        SimuRequest req = new SimuRequest(HttpMethod.Get, "/foo");
+
+
+        Assert.Throws<KeyNotFoundException>(() => table.Match(req));
+    }
+}
