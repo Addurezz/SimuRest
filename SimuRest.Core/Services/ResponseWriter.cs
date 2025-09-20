c65185c53756035b3390b5a1ceb1bdc7425d66e2
Wilhelm Jahn <wilhelmjahn2005@gmail.com>
2025-09-20 14:17:23 +0200

    add null check

--- SimuRest.Core/Services/ResponseWriter.cs
+++ SimuRest.Core/Services/ResponseWriter.cs
@@ -8,12 +8,15 @@ public class ResponseWriter
 {
     public void Write(HttpListenerContext ctx, SimuResponse response)
     {
+        if (response is null) return;
+        
         int status = response.StatusCode;
         string body = response.Body;
 
         ctx.Response.StatusCode = status;
         byte[] buffer = Encoding.UTF8.GetBytes(body);
 
+        ctx.Response.ContentType = "text/plain";
         ctx.Response.ContentLength64 = buffer.Length;
         ctx.Response.OutputStream.Write(buffer,0,buffer.Length);
         ctx.Response.OutputStream.Close();
