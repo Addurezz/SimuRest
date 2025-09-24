18c97b4f1ff968f4856f1e86e40e18a1c4b4e03b
Wilhelm Jahn (MacBook) <wilhelmjahn2005@gmail.com>
2025-09-24 17:27:49 +0200

    fix: changes to async in Start() and Stop() of SimuServer - Stop() is now gracefully shutting down the server - keeping track of all active requests

--- SimuRest.Core/Services/Http/SimuServer.cs
+++ SimuRest.Core/Services/Http/SimuServer.cs
@@ -10,7 +10,10 @@ public class SimuServer
     public Parser Parser;
     public HttpListener Listener;
 
+    private readonly object _tasksLock = new();
+    private List<Task> _activeRequests = new List<Task>();
     private int _port = 5000;
+    private CancellationTokenSource _cts = new();
 
     public int Port
     {
@@ -35,47 +38,63 @@ public class SimuServer
 
     public async Task Start()
     {
-        try
-        {
-            Listener.Prefixes.Add($"http://localhost:{Port}/");
-            Listener.Start();
+        // Setup the listener
+        Listener.Start();
+        Listener.Prefixes.Add($"http://localhost:{_port}/");
+
+        HttpListenerContext context;
         
-            while (Listener.IsListening)
+        // Accept incoming requests and handle them as long as the token doesnt cancel the inflow
+        while (Listener.IsListening && !_cts.IsCancellationRequested)
+        {
+            try
             {
-                HttpListenerContext context = await Listener.GetContextAsync(); // blockiert auf Request
-
-                _ = Task.Run(async () =>
-                {
-                    try
-                    {
-                        await Handler.Handle(context);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                });
+                // Get the request the user sent
+                context = await Listener.GetContextAsync();
             }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Oops, Something went wrong with the server.\n" + e);
-            this.Stop();
-        }
-    }
-    
-    public void Stop()
-    {
-        if (!Listener.IsListening) return;
+            catch (Exception e)
+            {
+                break;
+            }
+            
+            // Handle the user request on a different thread
+            Task requestTask = Handler.Handle(context);
+            
+            // Add the current request to keep track of active requests
+            lock(_tasksLock)
+            {
+                _activeRequests.Add(requestTask);
+            }
 
-        try
-        {
-            Listener.Stop();
-            Listener.Close();
+            _ = requestTask.ContinueWith(t =>
+            {
+                lock (_tasksLock)
+                {
+                    _activeRequests.Remove(t);
+                }
+            });
         }
-        catch (ObjectDisposedException)
+        
+        
+    }
+
+    public async Task Stop()
+    {
+        await _cts.CancelAsync();
+        
+        // stops the flow of incoming requests but does not close the connection yet
+        Listener.Stop();
+
+        // array of all the remaining active tasks
+        Task[] tasks;
+        lock (_tasksLock)
         {
-            // Already disposed – ignore
+            tasks = _activeRequests.ToArray();
         }
+        
+        // Await that every task in 'tasks' is finished
+        await Task.WhenAll(tasks);
+        // Close the listener
+        Listener.Close();
     }
 }
